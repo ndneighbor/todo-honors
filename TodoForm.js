18289decbb3fcaa12df69b4869216691ca105732
Angelo Saraceno <asara019@fiu.edu>
2017-05-09 13:44:31 -0400

    stuff

---
+++ TodoForm.js
@@ -0,0 +1,18 @@
+const TodoForm = ({addTodo}) => {
+
+    let input;
+
+    return (
+        <div>
+        <input ref={node => {
+            input = node;
+        }} />
+        <button onClick={() => {
+            addTodo(input.value);
+            input.value = '';
+        }}>
+            +
+        </button>
+        </div>
+    );
+};
